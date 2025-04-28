35335bf69ed6452515336d9b7866111879000bab
Darapureddy Manikanta <darapureddymanikanta8@gmail.com>
2025-04-28 15:20:39 +0530

    port changed

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def result():
     return render_template('result.html', label=label)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
 
